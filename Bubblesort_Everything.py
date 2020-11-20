1cd4c478c14d1a2fe6bf94987752fa719f8d7a42
Hussain <60139669+hussain2802@users.noreply.github.com>
2020-11-20 23:51:47 +0500

    Update Bubblesort_Everything.py

--- Bubblesort_Everything.py
+++ Bubblesort_Everything.py
@@ -1,18 +1,19 @@
 height = [45, 67, 23, 45, 21, 78, 90, 11, 21]
-maxsize = len(height)
+maxsize = len(height)-1
 counter = 0
 
 swapped = True
 while swapped == True:
     swapped = False
-    for count in range(maxsize-1):
+    for count in range(maxsize):
         if height[count]>height[count+1]:
             height[count],height[count+1]=height[count+1],height[count]
             swapped = True
+            pos=count
         counter=counter+1
-    maxsize=maxsize-1
+    maxsize=pos
 
-for i in range(9):
+for i in range(len(height)):
     print(i+1,': ',height[i])
 
 print('The algo analysis : ',counter)
